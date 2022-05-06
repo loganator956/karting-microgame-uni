f2560626459163a0de1acc3cce7656bb07804df2
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-05-06 19:16:38 +0100

    cool

---
+++ Assets/Boost.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KartGame.KartSystems;
+
+public class Boost : MonoBehaviour
+{
+    private float _boostCountDown = 1f;
+    public float BoostCountDown
+    {
+        get 
+        {
+            return _boostCountDown;
+        }
+        set
+        {
+            _boostCountDown = Mathf.Clamp01(value);
+        }
+    }
+    public float BoostReduceRate = 0.2f;
+    public AnimationCurve BoostCurve;
+    public float TopSpeedMultiplier = 2f, AccelerationMultiplier = 3f;
+
+    private ArcadeKart _kart;
+
+    private float _kartOriginalAcceleration, _kartOriginalTopSpeed;
+
+    void Awake()
+    {
+        _kart = GetComponent<ArcadeKart>();
+        _kartOriginalAcceleration = _kart.baseStats.Acceleration;
+        _kartOriginalTopSpeed = _kart.baseStats.TopSpeed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BoostCountDown > 0)
+        {
+            BoostCountDown -= Time.deltaTime * BoostReduceRate;
+            // _kartOriginalAcceleration + (_kartOriginalAcceleration * AccelerationMultiplier * BoostCurve.Evaluate(BoostCountDown));
+            _kart.baseStats.Acceleration = Mathf.Max(_kartOriginalAcceleration, _kartOriginalAcceleration * AccelerationMultiplier * BoostCurve.Evaluate(BoostCountDown));
+            _kart.baseStats.TopSpeed = _kartOriginalTopSpeed + (_kartOriginalTopSpeed * TopSpeedMultiplier * BoostCurve.Evaluate(BoostCountDown));
+        }
+    }
+}
