6c3e5410f066a80a3fcc78674aea714381594518
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-05-06 18:50:22 +0100

    Got AI drivers driving successfully

--- Assets/AIInput.cs
+++ Assets/AIInput.cs
@@ -22,9 +22,12 @@ public class AIInput : KartGame.KartSystems.BaseInput
 
     int currentIndex = 0;
 
+    Vector3 _targetDelta;
+
     void Update()
     {
-        Vector3 targetDelta = targets[currentIndex].transform.position - transform.position;
+        Vector3 targetDelta = (targets[currentIndex].transform.position - transform.position).normalized;
+        _targetDelta = targetDelta;
         targetDelta.y = 0;
         Vector3 forwards = transform.forward;
         forwards.y = 0;
@@ -38,14 +41,28 @@ public class AIInput : KartGame.KartSystems.BaseInput
             }
         }
         float angle = Vector3.SignedAngle(forwards, targetDelta, Vector3.up);
-        print(angle);
-        Steer = Mathf.Clamp(angle, -1f, 1f);
-        // print(Steer);
+        Steer = angle / 30f;
+        Steer = Mathf.Clamp(Steer, -1f, 1f);
+        print(Steer);
         Accelerate = true;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.DrawSphere(targets[currentIndex].transform.position, 2f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + _targetDelta * 2);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 2);
+
+        Vector3 minTurnAngle = Quaternion.AngleAxis(-30, Vector3.up) * transform.forward;
+        Vector3 maxTurnAngle = Quaternion.AngleAxis(30, Vector3.up) * transform.forward;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + minTurnAngle * 2);
+        Gizmos.DrawLine(transform.position, transform.position + maxTurnAngle * 2);
+
+        Gizmos.color = Color.cyan;
+        Vector3 steerAngle = Quaternion.AngleAxis(30 * Steer, Vector3.up) * transform.forward;
+        Gizmos.DrawLine(transform.position, transform.position + steerAngle);
     }
 }
