1119b297a88c2abebe1f9987205a5e7ce30bfb21
loganator956 <65763483+loganator956@users.noreply.github.com>
2022-05-11 17:37:00 +0100

    imported uni work

--- Assets/WeaponFire.cs
+++ Assets/WeaponFire.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class WeaponFire : MonoBehaviour
 {
     public int ammo = 0;
+    public bool isPlayer = false;
     void Update()
     {
         Collider[] cols = Physics.OverlapSphere(transform.position, 30f);
-        TimelinePlayer closestKart = null;
+        Enemy closestKart = null;
         float closestKartDistance = 999f;
 
         foreach (Collider col in cols)
         {
             Rigidbody rb = col.attachedRigidbody;
             if (!rb) { continue; };
-            TimelinePlayer kart = rb.GetComponent<TimelinePlayer>();
+            Enemy kart = rb.GetComponent<Enemy>();
             if (kart)
             {
                 float distance = Vector3.Distance(col.transform.position, transform.position);
@@ -28,29 +29,42 @@ public class WeaponFire : MonoBehaviour
             }
         }
         try { transform.LookAt(closestKart.transform.position, Vector3.up); } catch (System.NullReferenceException) { };
-        if (ammo > 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        if (isPlayer && ammo > 0 && Input.GetKeyDown(KeyCode.Mouse0))
         {
+            ammo--;
+            if (ammo <= 0)
+            {
+                DisableWeapon();
+            }
             RaycastHit rayHit;
             if (Physics.Raycast(transform.position + transform.forward, transform.forward, out rayHit, 30f))
             {
-                TimelinePlayer timeliner = rayHit.collider.attachedRigidbody.gameObject.GetComponent<TimelinePlayer>();
-                if (timeliner)
+                Instantiate(line, Vector3.zero, Quaternion.identity);
+                LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+                lineRenderer.SetPositions(new Vector3[] { transform.position, rayHit.point });
+                if (!rayHit.collider.attachedRigidbody) { return; };
+                Collider[] colliders = Physics.OverlapSphere(rayHit.point, 3f);
+                foreach (Collider col in colliders)
                 {
-                    timeliner.SlowDownLengthCounter = 20;
-                    ammo--;
-                    if (ammo <= 0)
-                    {
-                        DisableWeapon();
-                    }
+                    col.attachedRigidbody.AddExplosionForce(2000f, rayHit.point, 4f);
                 }
+
+                Debug.Log(rayHit.transform.name);
+                // Enemy timeliner = rayHit.collider.attachedRigidbody.gameObject.GetComponent<Enemy>();
+                // if (timeliner)
+                // {
+                //     // hit
+                //     timeliner.AddDamage();
+                // }
             }
         }
     }
+    public GameObject line;
 
     public void EnableWeapon()
     {
         ammo = 20;
-        foreach(MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
+        foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
         {
             renderer.enabled = true;
         }
@@ -58,7 +72,7 @@ public class WeaponFire : MonoBehaviour
 
     public void DisableWeapon()
     {
-        foreach(MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
+        foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
         {
             renderer.enabled = false;
         }
